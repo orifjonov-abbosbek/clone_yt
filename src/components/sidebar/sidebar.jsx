c1f42a579ad2ed24aebbf29fc5f685b62892eed7
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-04-23 12:55:15 +0500

    sidebar added

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import './sidebar.scss' 
+import React from "react";
+import "./sidebar.scss";
 import {
   MdSubscriptions,
   MdExitToApp,
-  MdThumpUp,
+  MdThumbUpAlt,
   MdHistory,
   MdLibraryBooks,
   MdHome,
   MdSentimentDissatisfied,
-} from 'react-icons/md'
+} from "react-icons/md";
 const Sidebar = () => {
   return (
-    <div className='border vorder-danger sidebar'>sidebar</div>
-  )
-}
+    <nav className="border border-danger sidebar">
+      <ul>
+        <li>
+          <MdHome size={23} /> <span>Home</span>{" "}
+        </li>
+        <li>
+          <MdSubscriptions size={23} /> <span>Subscription</span>{" "}
+        </li>
+        <li>
+          <MdThumbUpAlt size={23} /> <span>LikedVideos</span>{" "}
+        </li>
+        <li>
+          <MdHistory size={23} /> <span>History</span>{" "}
+        </li>
 
-export default Sidebar
+
+        <li>
+          <MdLibraryBooks size={23} />
+          <span>Library</span>
+        </li>
+        <li>
+          <MdSentimentDissatisfied size={23} />
+          <span>I don`t know</span>
+        </li>
+
+        <hr />
+
+        <li>
+          {" "}
+          <MdExitToApp size={23} />
+          <span>Log out</span>
+        </li>
+        <hr />
+      </ul>
+    </nav>
+  );
+};
+
+export default Sidebar;

2f261d60223cffd6bb396976ce54030dfe03d54d
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-04-23 18:16:49 +0500

    AUTH REDUCER ADDED

--- src/redux/actions/auth.action.jsx
+++ src/redux/actions/auth.action.jsx
@@ -1,11 +1,44 @@
 import { auth, provider } from "../../firebase";
 
 import { signInWithPopup } from "firebase/auth";
+import {
+    
+  LOAD_PROFILE,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+} from "../actionsTypes";
 
 export const Login = () => async (dispatch) => {
   try {
+    dispatch({
+      type: LOGIN_REQUEST,
+    });
     const res = await signInWithPopup(auth, provider);
 
     console.log(res);
-  } catch (error) {}
+
+    const accessToken = res.accessToken;
+    const profile = {
+      name: res.user.displayName,
+      photoURL: res.user.photoURL,
+    };
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: accessToken,
+    });
+
+    dispatch({
+      type: LOAD_PROFILE,
+      payload: profile,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.message,
+    });
+  }
 };
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,16 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { authReducer } from "./reducers/auth.reducer";
 
-const initialState = {
-  name: "someone",
-  age: "21",
-};
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
 const reducer = (initialState) => initialState;
 
 const store = createStore(
-  reducer,
-  initialState,
+  rootReducer,
+  {},
   composeWithDevTools(applyMiddleware(thunk))
 );
 export default store;
